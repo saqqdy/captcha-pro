f21fad441d8dd51cdb634223b10d6cb620e82c9e
saqqdy <saqqdy@qq.com>
2026-08-09 23:19:09 +0800

    refactor: update sync script to iterate over package names and remove unused publish-test workflow

--- scripts/sync.ts
+++ scripts/sync.ts
@@ -1,3 +1,24 @@
 import { execSync } from 'node:child_process'
 
-execSync('curl -X PUT -d "sync_upstream=true" "https://registry-direct.npmmirror.com/captcha-pro/sync"')
+const packages = [
+	'core',
+	'mp-shared',
+	'react',
+	'taro-react',
+	'taro-vue',
+	'taro-vue2',
+	'uniapp-vue',
+	'uniapp-vue2',
+	'vue',
+	'vue2',
+	'weixin',
+]
+
+for (const pkg of packages) {
+	execSync(
+		`curl -sX PUT -d "sync_upstream=true" "https://registry-direct.npmmirror.com/@captcha-pro/${pkg}/sync"`,
+		{
+			stdio: 'inherit',
+		}
+	)
+}
